e167e9559d4949e078406763eee4937376472af6
Mystique <13oct08@quantumfoam.uni.cc>
2019-05-02 16:38:44 +0200

    Fixed some bugs, LGTM

--- helper_scripts/deploy.sh
+++ helper_scripts/deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -ex
 
-# Change BUCKET_NAME to a bucket you own for use by CloudFormation
-BUCKET_NAME="sam-templates-010"
+#----- Change these parameters to suit your environment -----#
+AWS_PROFILE="default"
+BUCKET_NAME="sam-templates-010" # bucket must exist in the SAME region the deployment is taking place
 TEMPLATE_NAME="serverless-cloudwatch-log-retention-policy.yaml"
 STACK_NAME="set-cloudwatch-logs-retention"
 OUTPUT_DIR="./outputs/"
 PACKAGED_OUTPUT_TEMPLATE="${OUTPUT_DIR}${STACK_NAME}-packaged-template.yaml"
+#----- End of user parameters  -----#
 
 aws cloudformation package \
-    --template-file ${TEMPLATE_NAME} \
-    --s3-bucket $BUCKET_NAME \
-    --output-template-file ${PACKAGED_OUTPUT_TEMPLATE}
+    --template-file "${TEMPLATE_NAME}" \
+    --s3-bucket "${BUCKET_NAME}" \
+    --output-template-file "${PACKAGED_OUTPUT_TEMPLATE}"
 
 aws cloudformation deploy \
-    --template-file ${PACKAGED_OUTPUT_TEMPLATE} \
-    --stack-name ${STACK_NAME} \
+    --profile "${AWS_PROFILE}" \
+    --template-file "${PACKAGED_OUTPUT_TEMPLATE}" \
+    --stack-name "${STACK_NAME}" \
     --capabilities CAPABILITY_IAM
